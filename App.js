92abec91a991a22a7baed67b01f5d0188fddf0c0
Sasa Mora Roca <sabata79@hotmail.com>
2023-09-26 17:36:03 +0300

    added comment

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { StatusBar, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import styles from './styles/styles';
 import React, { useState } from 'react';
 import { Pressable } from 'react-native';
 
 export default function App() {
-
 
   const values = ["Option1", "Option2", "Option3", "Option4"];
   const [value, setValue] = useState(values[0]);
@@ -13,11 +12,12 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.label}>Select one option:</Text>
       <ButtonSelector values={values} selectedValue={value} selectionChanged={setValue} />
-      <Text style={styles.result}>You selected value: {value}</Text>
+      <Text style={styles.result}>You selected: {value}</Text>
     </View>
   );
 }
 
+/* New style whit function (2023) */
 function ButtonSelector({ values, selectedValue, selectionChanged }) {
 
     return (
@@ -31,3 +31,17 @@ function ButtonSelector({ values, selectedValue, selectionChanged }) {
         </View>
     );
 }
+
+/* This is old style */
+/* const ButtonSelector = ({values, selectedValue,selectionChanged}) =>
+    return(
+        <View style={styles.buttonrow}>
+            {values.map((value) => (
+                <Pressable key={value} onPress={() => selectionChanged(value)}>
+                    <Text
+                    style={[styles.button, value==selectedValue && styles.selectedButton]}>{value}</Text>
+                </Pressable>
+            ))}
+        </View>
+      )
+   */
